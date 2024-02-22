97b26a48735485f16c863f6f313e3c97d7ba2264
Edouard Desgrée <edouard.desgree@gmail.com>
2024-02-22 06:09:53 +0100

    setup

---
+++ src/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import './index.css';
+import About from "./pages/About";
+import Home from "./pages/Home";
+
+function App() {
+    return (
+        <BrowserRouter>
+            <header>
+                <Link className="site-logo" to="/">#VanLife</Link>
+                <nav>
+                    <Link to="/about">About</Link>
+                </nav>
+            </header>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+ReactDOM
+    .createRoot(document.getElementById('root'))
+    .render(<App />);
